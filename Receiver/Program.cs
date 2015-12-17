091171d262ba93fdcb9e4e27624011b5cb7dbfac
Deepak Agnihotri <deepak2007in@gmail.com>
2015-12-17 17:35:15 +0530

    Initial Commit for experimenting Azure Service Bus

---
+++ Receiver/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Receiver
+{
+    using System;
+    using Microsoft.Azure;
+    using Microsoft.ServiceBus.Messaging;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect to a service
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var client = QueueClient.CreateFromConnectionString(connectionString: connectionString, path: "TestQueue");
+
+            // Configure the callback options
+            var options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            // Callback to handle received messages
+            client.OnMessage(message =>
+                {
+                    try
+                    {
+                        // Process message from the queue
+                        Console.WriteLine("Body: " + message.GetBody<string>());
+                        Console.WriteLine("MessageID: " + message.MessageId);
+                        Console.WriteLine("Test Property: " + message.Properties["TestProperty"]);
+                        message.Complete();
+                    }
+                    catch(Exception)
+                    {
+                        message.Abandon();
+                    }
+                }, options);
+        }
+    }
+}
---
+++ ServiceBus/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Sender
+{
+    using System;
+    using Microsoft.Azure;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configure queue settings.
+            var queueDescription = new QueueDescription("TestQueue");
+            queueDescription.MaxSizeInMegabytes = 5120;
+            queueDescription.DefaultMessageTimeToLive = new TimeSpan(0, 1, 0);
+
+            // Create a new queue with custom settings.
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            if (!namespaceManager.QueueExists("TestQueue"))
+            {
+                namespaceManager.CreateQueue(description: queueDescription);
+            }
+
+            // Sending message to the queue
+            var client = QueueClient.CreateFromConnectionString(connectionString, "TestQueue");
+            for (var index = 0; index < 5; index++)
+            {
+                var message = new BrokeredMessage("Test Message " + index);
+                message.Properties["TestProperty"] = "TestValue";
+                message.Properties["Message Number"] = index;
+                client.Send(message: message);
+            }
+        }
+    }
+}
